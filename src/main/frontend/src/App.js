0ab5b3d788bce2ab3939f62245c05d89c78f12fb
sungslee <k8440009@gmail.com>
2022-11-05 22:50:20 +0900

    add bootstrap

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {useEffect, useState} from "react";
 import axios from "axios";
 
+import Table from 'react-bootstrap/Table'
+
 function App() {
   const [hello, setHello] = useState('')
 
@@ -11,8 +13,27 @@ function App() {
         .catch(err => console.log(err));
   });
   return (
-      <div>
-        백엔드에서 가져온 데이터입니다. : {hello}
+      <div className={"div_1"}>
+          <div className={"div_2"}>
+              <Table striped bordered hover>
+                  <thead className={"thead_1"}>
+                  <tr className={"tr_1"}>
+                      <th>게시글번호</th>
+                      <th>제목</th>
+                      <th>작성자</th>
+                      <th>최종수정일</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>hello</td>
+                        <td>김애용</td>
+                        <td>20221105</td>
+                    </tr>
+                  </tbody>
+              </Table>
+          </div>
       </div>
   );
 }
